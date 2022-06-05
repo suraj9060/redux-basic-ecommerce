8ce9c89cb66abcef0642bb5d39f6921e710bebf7
suraj9060 <87435542+suraj9060@users.noreply.github.com>
2022-06-06 01:23:26 +0530

    add to cart all funcnality done using redux and data coming dinamicaly

--- src/components/CartDetails.jsx
+++ src/components/CartDetails.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Table } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 
 const CartDetails = () => {
 
-  
+  const [data , setData] = useState([])
+ console.log(data)
+  const { id } = useParams()
+  // console.log(id)
+    const getData = useSelector((state) => state.cartreducer.Carts);
+    // console.log(getData);
 
+  const compare = () => {
+    let comparedata = getData.filter((e) => {
+      return e.id == id
+    })
+    setData(comparedata)
+  }
 
+  useEffect(() => {
+    compare()
+  },[id])
 
 
   return (
@@ -13,70 +29,82 @@ const CartDetails = () => {
       <h2 className="text-center">Item Details Page</h2>
       <section className="container mt-3">
         <div className="iteamsdetails">
-          <div className="items_img">
-            <img
-              src="https://b.zmtcdn.com/data/pictures/3/18514413/0a17b72e9fec52a3ca736f4c2ea3646f_o2_featured_v2.jpg"
-              alt=""
-            />
-          </div>
+          {
+            data.map((element) => {
+              return (
+                <>
+                  <div className="items_img">
+                    <img
+                      src={element.imgdata}
+                      alt=""
+                    />
+                  </div>
 
-          <div className="details">
-            <Table>
-              <tr>
-                <td>
-                  <p>
-                    <strong>Restaurant</strong> : masai School
-                  </p>
-                  <p>
-                    <strong>Price</strong> : ₹ 500
-                  </p>
-                  <p>
-                    <strong>Dishex</strong> : masai School , biryani , Mughli
-                  </p>
-                  <p>
-                    <strong>Total</strong> : ₹ 500
-                  </p>
-                </td>
-                <td>
-                  <p>
-                    <strong>Rating :</strong>{" "}
-                    <span
-                      style={{
-                        background: "green",
-                        color: "#fff",
-                        padding: "2px 5px",
-                        borderRadius: "5px",
-                      }}
-                    >
-                      ★3.5
-                    </span>
-                  </p>
+                  <div className="details">
+                    <Table>
+                      <tr>
+                        <td>
+                          <p>
+                            <strong>Restaurant</strong> : {element.rname}
+                          </p>
+                          <p>
+                            <strong>Price</strong> : ₹ {element.price}
+                          </p>
+                          <p>
+                            <strong>Dishex</strong> : {element.address}
+                          </p>
+                          <p>
+                            <strong>Total</strong> : ₹ 500
+                          </p>
+                        </td>
+                        <td>
+                          <p>
+                            <strong>Rating :</strong>{" "}
+                            <span
+                              style={{
+                                background: "green",
+                                color: "#fff",
+                                padding: "2px 5px",
+                                borderRadius: "5px",
+                              }}
+                            >
+                              ★{element.rating}
+                            </span>
+                          </p>
 
-                  <p>
-                    <strong>Order Review</strong>{" "}
-                    <span>1175 + Order Placed From Here Recently</span>
-                  </p>
+                          <p>
+                            <strong>Order Review</strong>{" "}
+                            <span>{element.somedata}</span>
+                          </p>
 
-                  <p>
-                    <strong>Rating :</strong>{" "}
-                    <span>
-                      <i
-                        className="fas fa-trash"
-                        style={{
-                          
-                          color: "red",
-                          padding: "10px",
-                          borderRadius: "5px",
-                          cursor: 'pointer',
-                          fontSize:25
-                        }}
-                      ></i>
-                    </span>
-                  </p>
-                </td>
-              </tr>
-            </Table>
-          </div>
+                          <p>
+                            <strong>Rating :</strong>{" "}
+                            <span>
+                              <i
+                                className="fas fa-trash"
+                                style={{
+                                  color: "red",
+                                  padding: "10px",
+                                  borderRadius: "5px",
+                                  cursor: "pointer",
+                                  fontSize: 25,
+                                }}
+                              ></i>
+                            </span>
+                          </p>
+                        </td>
+                      </tr>
+                    </Table>
+                  </div>
+                </>
+              );
+            })
+        }
+
+
+
+
+          
         </div>
       </section>
     </div>
